eb1dd1632899cedb61da9cb541cf557f799b7528
Alazar Tadesse <mealazartadesse@gmail.com>
2022-02-21 06:43:06 +0300

    finished layout

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,13 +1,24 @@
 import Header from "./Header";
 import Robot from "../assets/img/robot.png";
 import ViewServices from "./ViewServices";
+import {
+  eclipse1,
+  eclipse2,
+  eclipse3,
+  eclipse4,
+  eclipse5,
+  eclipse6,
+  eclipse7,
+} from "../assets/images";
 
 export default function Landing() {
   return (
-    <div>
+    <div className="App bg-black px-20 pb-20 rounded-3xl">
       <Header />
       <div className="flex items-center">
-        <div>
+        <img className="eclipse-7" src={eclipse7} alt="" />
+        <img className="eclipse-6" src={eclipse6} alt="" />
+        <div className="w-98">
           <div className="flex">
             <p
               className="text-white text-3xl font-bold"
@@ -17,10 +28,11 @@ export default function Landing() {
             </p>
             &nbsp;
             <span className="text-white text-3xl font-bold">
-              the fascinating world of VR
+              the fascinating
             </span>
           </div>
-          <p className="text-white">
+          <span className="text-white text-3xl font-bold">world of VR</span>
+          <p className="text-white py-5">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut cursus
             ac ipsum sed rhoncus. Nam euismod auctor ipsum. Etiam interdum nulla
             neque, eu elementum arcu fermentum vel.
@@ -30,7 +42,12 @@ export default function Landing() {
           </button>
         </div>
         <div>
-          <img src={Robot} alt="" />
+          <img className="eclipse-3" src={eclipse3} alt="" />
+          <img className="eclipse-1" src={eclipse1} alt="" />
+          <img className="eclipse-2" src={eclipse2} alt="" />
+          <img className="hero-img" src={Robot} alt="" />
+          <img className="eclipse-4" src={eclipse4} alt="" />
+          <img className="eclipse-5" src={eclipse5} alt="" />
         </div>
       </div>
       <ViewServices />
--- src/components/ViewServices.js
+++ src/components/ViewServices.js
@@ -5,22 +5,28 @@ import Finance from "../assets/img/finance.png";
 
 export default function ViewServices() {
   return (
-    <div className="flex justify-center space-x-10">
-      <div className="flex flex-col space-y-100 items-center">
+    <div className="services-bg flex justify-center space-x-10 p-5 rounded-3xl">
+      <div className="service-bg flex flex-col items-center">
         <img className="service-img" src={Glasses} alt="" />
-        <p className="text-white">Experience the world through VR glasses</p>
+        <p className="text-white text-center">
+          Experience the world through VR glasses
+        </p>
       </div>
-      <div className="flex flex-col space-y-100 items-center w-22">
+      <div className="service-bg flex flex-col space-y-100 items-center">
         <img className="service-img" src={Resolution} alt="" />
-        <p className="text-white">View all your surrounding with 360</p>
+        <p className="text-white text-center">
+          View all your surrounding with 360
+        </p>
       </div>
-      <div className="flex flex-col space-y-100 items-center w-22">
+      <div className="service-bg flex flex-col space-y-100 items-center">
         <img className="service-img" src={Game} alt="" />
-        <p className="text-white">Incredible gaming experience</p>
+        <p className="text-white text-center">Incredible gaming experience</p>
       </div>
-      <div className="flex flex-col space-y-100 items-center w-22">
+      <div className="service-bg flex flex-col space-y-100 items-center">
         <img className="service-img" src={Finance} alt="" />
-        <p className="text-white">Making meetings and tours easier</p>
+        <p className="text-white text-center">
+          Making meetings and tours easier
+        </p>
       </div>
     </div>
   );
