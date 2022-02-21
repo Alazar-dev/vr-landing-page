338c63730ee90c74c3dcf19c018bb476dc6e0a9a
Alazar Tadesse <mealazartadesse@gmail.com>
2022-02-21 06:42:36 +0300

    added images and exported

---
+++ src/assets/images.js
@@ -0,0 +1,7 @@
+export const eclipse1 = require("../assets/img/Ellipse-1.png");
+export const eclipse2 = require("../assets/img/Ellipse-2.png");
+export const eclipse3 = require("../assets/img/Ellipse-3.png");
+export const eclipse4 = require("../assets/img/Ellipse-4.png");
+export const eclipse5 = require("../assets/img/Ellipse-5.png");
+export const eclipse6 = require("../assets/img/Ellipse-6.png");
+export const eclipse7 = require("../assets/img/Ellipse-7.png");
